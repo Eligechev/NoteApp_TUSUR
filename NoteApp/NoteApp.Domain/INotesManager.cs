7a3117e910bca4f184632af4152fdf193dd0be0a
Виктор Елигечев <vitec949@gmail.com>
2020-10-30 09:28:09 +0700

    add domain layer

---
+++ NoteApp/NoteApp.Domain/INotesManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NoteApp.Domain
+{
+    using NoteApp.Domain.Models;
+
+    public interface INotesManager
+    {
+        void AddNote(NoteModel note);
+    }
+
+    public class NotesManager : INotesManager
+    {
+        public void AddNote(NoteModel note)
+        {
+
+        } 
+    }
+}
---
+++ NoteApp/NoteApp.Domain/Models/NoteModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteApp.Domain.Models
+{
+    public class NoteModel
+    {
+        public int ID { get; set; }
+
+        public string NoteMessage { get; set; }
+    }
+}
